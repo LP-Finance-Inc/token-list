88ab98f39fdb9e557da21a440e614d392eea1273
Joohwan Lee <aperire0402@gmail.com>
2023-02-20 11:00:17 -0500

    add

---
+++ scripts/build_list.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+from io import StringIO
+import json
+
+"""
+FORMAT:
+{
+    TOKEN_MINT: {
+        name: NAME,
+        symbol: SYMBOL,
+        decimals: DECIMALS,
+        logo: LOGO_URI,
+    }
+}
+"""
+JUP_VALIDATED_TOKENS_URI = "https://raw.githubusercontent.com/jup-ag/token-list/main/validated-tokens.csv"
+
+response = requests.get(JUP_VALIDATED_TOKENS_URI).text
+
+token_list_csv = pd.read_csv(StringIO(response))
+
+name_array = token_list_csv["Name"].tolist()
+symbol_array = token_list_csv["Symbol"].tolist()
+mint_array = token_list_csv["Mint"].tolist()
+decimals_array = token_list_csv["Decimals"].tolist()
+logo_array = token_list_csv["LogoURI"].tolist()
+
+token_list_json = {
+    mint_array[i]: {
+        "name": name_array[i],
+        "symbol": symbol_array[i],
+        "decimals": decimals_array[i],
+        "logo": logo_array[i]
+    }
+    for i in range(len(name_array))
+}
+
+with open("../token-list.json", "w") as file:
+
+    json.dump(token_list_json, file, indent=4)
