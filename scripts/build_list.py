b2a54fd39dace06e7045cd252472fd5797d8be27
aperire <aperire0402@gmail.com>
2023-05-01 00:47:45 -0400

    build_list to add legacy

--- scripts/build_list.py
+++ scripts/build_list.py
@@ -16,11 +16,8 @@ FORMAT:
 }
 """
 JUP_VALIDATED_TOKENS_URI = "https://raw.githubusercontent.com/jup-ag/token-list/main/validated-tokens.csv"
-
 response = requests.get(JUP_VALIDATED_TOKENS_URI)
-
 assert response.status_code == 200
-
 token_list_csv = pd.read_csv(StringIO(response.text))
 
 name_array = token_list_csv["Name"].tolist()
@@ -28,6 +25,25 @@ symbol_array = token_list_csv["Symbol"].tolist()
 mint_array = token_list_csv["Mint"].tolist()
 decimals_array = token_list_csv["Decimals"].tolist()
 logo_array = token_list_csv["LogoURI"].tolist()
+
+SOLANA_TOKENS_URI = "https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json"
+response_solana = requests.get(SOLANA_TOKENS_URI)
+assert response_solana.status_code == 200
+solana_token_list = response_solana.json()["tokens"]
+
+for obj in solana_token_list:
+    if obj["chainId"] == 101:
+        solana_name = obj["name"]
+        solana_symbol = obj["symbol"]
+        solana_mint = obj["address"]
+        solana_decimals = obj["decimals"]
+        solana_logo = obj["logoURI"]
+        if solana_mint not in mint_array:
+            name_array.append(solana_name)
+            symbol_array.append(solana_symbol)
+            mint_array.append(solana_mint)
+            decimals_array.append(solana_decimals)
+            logo_array.append(solana_logo)
 
 token_list_json = {
     mint_array[i]: {
@@ -40,5 +56,4 @@ token_list_json = {
 }
 
 with open("./token-list.json", "w") as file:
-
     json.dump(token_list_json, file, indent=4)
